45e668660a1e82f957dfb1259a2fc2c032e6946f
Michael Bradley <michaelmbrad@gmail.com>
2023-01-27 17:18:45 -0500

    Add json GET and caching

--- rofi-cafmenu.py
+++ rofi-cafmenu.py
@@ -1,16 +1,67 @@
+#!./venv/bin/python
+import enum
+import json
+import datetime
+
+import requests
 import rofi_menu
-from xdg import xdg_cache_home
-
-CACHE = xdg_cache_home() / "rofi-cafmenu"
+import xdg
 
 
-class MealsMenu(rofi_menu.Menu):
+class Meal(enum.Enum):
+    BREAKFAST = 2082
+    LUNCH = 2084
+    DINNER = 2085
+
+
+def get_meals(start_day: datetime.date, num_days: int):
+    meals = []
+    for day in range(num_days):
+        delta = datetime.timedelta(days=day)
+        for meal in Meal:
+            meals.append(get_meal(start_day + delta, meal))
+    return meals
+
+
+def get_meal(day: datetime.date, meal: Meal):
+    return get_cached_meal(day, meal) or get_online_meal(day, meal)
+
+
+CACHE = xdg.xdg_cache_home() / "rofi-cafmenu"
+CACHE.mkdir(parents=True, exist_ok=True)
+
+
+def get_file(day: datetime.date, meal: Meal):
+    return CACHE / f"{day.isoformat()}-{meal.name}.json"
+
+
+def get_cached_meal(day: datetime.date, meal: Meal):
+    try:
+        with open(get_file(day, meal), "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
+
+
+API_URL = "https://carleton.campusdish.com/api/menu/GetMenus"
+PARAMS = "?locationId=5087&storeIds=&mode=Daily&date={}/{}/{}&time=&periodId={}&fulfillmentMethod="
+
+
+def get_online_meal(day: datetime.date, meal: Meal):
+    meal_data = requests.get(API_URL + PARAMS.format(day.month, day.day, day.year, meal.value)).json()
+    with open(get_file(day, meal), "w") as file:
+        json.dump(meal_data, file)
+    return meal_data
+
+
+class MealMenu(rofi_menu.Menu):
     prompt = "Meals"
-    items = [
-        rofi_menu.TouchpadItem()
-    ]
+
+
+class DishMenu(rofi_menu.Menu):
+    prompt = "Dishes"
 
 
 if __name__ == "__main__":
-    print(CACHE.glob("*"))
-    # rofi_menu.run(MealsMenu(), rofi_version="1.6")
+    get_meals(datetime.date.today(), 3)
+    # rofi_menu.run(MealMenu(), rofi_version="1.6")
