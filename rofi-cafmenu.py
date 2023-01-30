91895f634ad65f0e01243ee91f923b3518c7f189
Michael Bradley <michaelmbrad@gmail.com>
2023-01-30 03:58:29 -0500

    Run cacher in background whenever Rofi script is run

--- rofi-cafmenu.py
+++ rofi-cafmenu.py
@@ -1,8 +1,13 @@
 #!/home/mbradley/dev/rofi-cafmenu/venv/bin/python
+import pathlib
+import subprocess
+
 import rofi_menu
 
 import rofi_menus
 
 
 if __name__ == "__main__":
+    cacher = pathlib.Path(__file__).parent / "cache-upcoming-meals.py"
+    subprocess.Popen([cacher], stdout=subprocess.DEVNULL)
     rofi_menu.run(rofi_menus.MealMenu(), rofi_version="1.6")
